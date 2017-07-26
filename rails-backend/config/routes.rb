f74c20db7e12e33aec8590f2926e2e07d412d424
Simon Cozens <simon@simon-cozens.org>
2017-07-26 11:23:58 +1000

    Move API stuff to an api namespace. We’re not going to end here, but it makes it possible to Heroku-deploy this as a single app.

--- rails-backend/config/routes.rb
+++ rails-backend/config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  root to: "people#index"
-  resources :people do
-    member do
-      get 'follow'
-      get 'unfollow'
+  namespace :api do
+    resources :people do
+      member do
+        get 'follow'
+        get 'unfollow'
+      end
+      collection do
+        get 'following'
+      end
     end
-    collection do
-      get 'following'
-    end
+    match '/login', to: "auth#login", via: [:post]
   end
-  match '/login', to: "auth#login", via: [:post]
 end
